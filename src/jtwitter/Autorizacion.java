12a37383a2938f3e048663b8182fd14f9be8f5c0
BraisDiaz <braisdiaz@hotmail.com>
2017-06-01 09:12:07 +0200

    +Constructor sin parámetros +Método showTimeLine()

--- src/jtwitter/Autorizacion.java
+++ src/jtwitter/Autorizacion.java
@@ -1,10 +1,12 @@
 
 package jtwitter;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.Iterator;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
@@ -18,8 +20,45 @@ public class Autorizacion {
     private ConfigurationBuilder cb;
     
     private Twitter twitter = TwitterFactory.getSingleton();
+
+    public Autorizacion() {
+    }
     
-    
+    public void showTimeLine(){
+        
+        List<Status> statuses = null;
+
+        String mensajes = "Showing home timeline.\n";
+
+        int i = 0;
+        
+        Status aux = null;
+
+        try {
+
+            statuses = twitter.getHomeTimeline();
+
+        } catch (TwitterException ex) {
+
+            Logger.getLogger(JTwitter.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        Iterator<Status> it = statuses.iterator();
+
+        while (it.hasNext() && i < 5) {
+
+            aux = it.next();
+
+            mensajes += aux.getUser().getName() + " : " + aux.getText() + "\n-------\n";
+
+            i++;
+
+        }
+
+        JOptionPane.showMessageDialog(null, mensajes);
+        
+    }
     
  
 }
