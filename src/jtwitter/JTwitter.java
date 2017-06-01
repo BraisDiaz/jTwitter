a8ef6d45e01a36c1fdc1daeafc072b3ad4719468
BraisDiaz <braisdiaz@hotmail.com>
2017-06-01 08:54:17 +0200

    Creada rama withProperties

--- src/jtwitter/JTwitter.java
+++ src/jtwitter/JTwitter.java
@@ -24,44 +24,5 @@ public class JTwitter {
      */
     public static void main(String[] args) {
        
-        Autorizacion.conectarse();
-   
-       int opcion;
-    
-       do {
-
-  opcion = Integer.parseInt(JOptionPane.showInputDialog("1.Buscar Tweet \n 2.TimeLine \n 3.Nuevo Tweet \n 4.Enviar Mensaje \n 5.Seleccionar tweet"));
-
-    switch(opcion){
-
-        case 1:Autorizacion.buscarTweet(JOptionPane.showInputDialog("Introduzca el hashtag a buscar"));  
-
-            break;
-
-        case 2:Autorizacion.timeLine();
-
-            break;
-
-        case 3:Autorizacion.publicarTweet(JOptionPane.showInputDialog(null,"Introduzca lo que desee publicar"));
-
-            break;
-
-        case 4:Autorizacion.enviarMensaje(JOptionPane.showInputDialog("Introduce destinatario"),JOptionPane.showInputDialog("Introduce el mensaje"));
-
-            break;
-
-        case 5:Autorizacion.seleccionar();
-
-            break;
 
     }
-
-          }while(opcion!=0);
-    }
-}
-        
-        
-        
-    
-    
-
